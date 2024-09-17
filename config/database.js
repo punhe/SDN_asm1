063c4f45b0e0e5ce7a3bee583920f30f35f83aa9
Hung <lmhungfptqn@gmail.com>
2024-09-17 23:27:44 +0700

    Update

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/SimpleQuiz', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
