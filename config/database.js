6ef5dc1e60b9accb732cbaa960c7efbd7368d6ae
Hung <lmhungfptqn@gmail.com>
2024-09-18 08:43:11 +0700

    Update  config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// Đặt strictQuery ở đây, bên ngoài hàm connectDB
+mongoose.set('strictQuery', false);
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error('MONGODB_URI is not defined in the environment variables');
+    }
+    console.log('Attempting to connect with URI:', uri);
+    const conn = await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
